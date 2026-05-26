3e16dcb2866fa0d8fa25901af303711881d58090
nedarctic <nedarctic@gmail.com>
2026-05-26 13:26:13 +0300

    config registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { validationSchema } from './config/validation';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    PrismaModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
