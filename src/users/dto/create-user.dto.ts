facf057819aa907785096166820d03e2eea061fb
nedarctic <nedarctic@gmail.com>
2026-05-26 23:37:57 +0300

    optional user creation dto fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { IsEmail, IsString, MinLength, IsOptional, IsUUID, IsEnum } from "class-validator";
+import { UserRole, UserStatus } from "../../generated/prisma/enums";
 
 export class CreateUserDto {
     @IsString()
@@ -9,4 +10,16 @@ export class CreateUserDto {
 
     @MinLength(6)
     password!: string;
+
+    @IsOptional()
+    @IsUUID()
+    companyId?: string;
+
+    @IsOptional()
+    @IsEnum(UserRole)
+    role?: UserRole;
+
+    @IsOptional()
+    @IsEnum(UserStatus)
+    status?: UserStatus;
 }
