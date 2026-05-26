8cff8e9af3b5809197c8df846c4433f336acff4e
nedarctic <nedarctic@gmail.com>
2026-05-26 23:32:05 +0300

    users service functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async createUser(dto: CreateUserDto) {
+        return await this.prisma.user.create({ data: { ...dto } })
+    }
+
+    async removeUser(id: string) {
+        return await this.prisma.user.delete({ where: { id } })
+    }
+
+    async findUser(id?: string, email?: string) {
+
+        let user: any = null;
+
+        if (id) {
+            user = await this.prisma.user.findUnique({ where: { id } });
+        }
+
+        if (email) {
+            user = await this.prisma.user.findUnique({ where: { email } });
+        }
+
+        if (!user) {
+            throw new NotFoundException(
+                'User with the credential provided does not exist.'
+            );
+        }
+
+        return user;
+    }
+
+    async getUsers() {
+        return await this.prisma.user.findMany();
+    }
+
+    async updateUser(id: string, dto: UpdateUserDto){
+        try {
+            await this.prisma.user.update({
+                where: {id},
+                data: {...dto}
+            })
+        } catch(e){
+            throw new NotFoundException("User does not exist.")
+        }
+    }
+}
