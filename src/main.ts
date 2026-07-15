02690c5bee845a7461721444cb3303ebb3f755e5
nedarctic <nedarctic@gmail.com>
2026-07-15 13:56:21 +0300

    enable cors all origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
