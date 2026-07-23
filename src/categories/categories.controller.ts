d11a525582385a0108fb42b7b33b658e98234dc4
nedarctic <nedarctic@gmail.com>
2026-07-23 18:10:53 +0300

    categories mod

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { UserRole } from '../generated/prisma/enums';
 
+@UseGuards(JwtAuthGuard)
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+    constructor(
+        private readonly categoriesService: CategoriesService
+    ) { }
+
+    @UseGuards(RolesGuard)
+    @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
+    @Get(":companyId")
+    async createCategories(
+        @Param("companyId") companyId: string
+    ) { 
+        return await this.categoriesService.getCategories(companyId)
+    }
+
+    @Patch(":companyId/update-categories")
+    async updateCategories (
+        @Param("companyId") companyId: string,
+        @Body() categories: {categoryName: string, id: string}[]
+    ) {
+        return await this.categoriesService.editCategories(categories, companyId)
+    }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CompanyNotFoundException } from '../companies/exceptions/company-not-found.exception';
+import { CategoryNotFoundException } from './dto/category-not-found-exception';
+import { ExistingObjectReplication$ } from '@aws-sdk/client-s3';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(
+        private readonly prisma: PrismaService
+    ) { }
+
+    async getCategories(companyId: string) {
+        try {
+            const company = await this.prisma.company.findUnique({
+                where: {
+                    id: companyId
+                }
+            });
+
+            if (!company) {
+                throw new CompanyNotFoundException()
+            }
+
+            const categories = await this.prisma.category.findMany({
+                where: {
+                    companyId
+                }
+            });
+
+            return categories;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    async editCategories (categories: {categoryName: string, id: string}[], companyId: string) {
+        try {
+
+            const existingCategories = await this.prisma.category.findMany({
+                where: {
+                    companyId
+                }
+            });
+
+            const existingIdsSet = new Set<string>();
+            for(const category of existingCategories) {
+                existingIdsSet.add(category.id)
+            }
+
+            const newCategories = categories.filter(category => !existingIdsSet.has(category.id)).map(category => ({...category, companyId}));
+            const updatedCategories = categories.filter(category => existingIdsSet.has(category.id)).map(category => ({...category, companyId}));
+
+            await this.prisma.$transaction(
+                updatedCategories.map(category => this.prisma.category.update({
+                    where: {
+                        id: category.id
+                    },
+                    data: {
+                        categoryName: category.categoryName,
+                        companyId: category.companyId
+                    }
+                }))
+            );
+
+            await this.prisma.category.createMany({
+                data: newCategories
+            });
+
+            return {
+                message: "Category updates succeeded"
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
