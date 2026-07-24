7ef94714690b7e2a1b49ad745e96fcac0d307eb5
nedarctic <nedarctic@gmail.com>
2026-07-24 18:03:15 +0300

    email controller

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Param } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -15,5 +15,15 @@ export class EmailController {
     @Post()    
     async sendEmail(@Body() body: { to: string; subject: string; html: string }) {
         return await this.emailService.sendEmail(body.to, body.subject, body.html);
+    }
+
+    @UseGuards(RolesGuard)
+    @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
+    @Post("invites/:companyId")    
+    async sendInviteEmail(
+        @Param("companyId") companyId: string,
+        @Body() body: { to: string }
+    ) {
+        return await this.emailService.sendInviteMail(body.to, companyId);
     }
 }
