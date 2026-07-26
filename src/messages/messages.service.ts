7b07e3020f46393cb4bf7280f9273d1413e8eb58
nedarctic <nedarctic@gmail.com>
2026-07-26 19:13:17 +0300

    get incident messages

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuditLogService } from '../audit-log/audit-log.service';
 import { SenderType } from '../generated/prisma/enums';
+import { IncidentNotFoundException } from '../incidents/exceptions/incident-not-found.exception';
 
 @Injectable()
 export class MessagesService {
@@ -10,14 +11,37 @@ export class MessagesService {
         private readonly auditLog: AuditLogService,
     ) { }
 
+    // get messages
+    async getIncidentMessages(incidentId: string) {
+        try {
+            const incident = await this.prisma.incident.findUnique({
+                where: {
+                    id: incidentId
+                }
+            });
+
+            if (!incident) throw new IncidentNotFoundException(incidentId);
+
+            return await this.prisma.message.findMany({
+                where: {
+                    incidentId
+                }
+            });
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
     // create message
-    async createMessage(incidentId: string, content: string, senderType: SenderType) {
+    async createMessage(incidentId: string, content: string, senderType: SenderType, handlerId?: string) {
         try {
             return await this.prisma.message.create({
                 data: {
                     content,
                     incidentId,
-                    senderType
+                    senderType,
+                    userId: handlerId,
                 }
             })
         } catch (error) {
